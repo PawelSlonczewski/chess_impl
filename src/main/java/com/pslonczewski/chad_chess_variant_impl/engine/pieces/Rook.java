edb5e769c0cb67a598bb491ab288d3e0a780547e
PawelSlonczewski <p.slonczewski@student.uksw.edu.pl>
2024-12-22 18:15:48 +0100

    add rook figure

--- src/main/java/com/pslonczewski/chad_chess_variant_impl/engine/pieces/Rook.java
+++ src/main/java/com/pslonczewski/chad_chess_variant_impl/engine/pieces/Rook.java
@@ -8,15 +8,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class Bishop extends Piece {
+public class Rook extends Piece{
 
-    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = { -9, -7, 7, 9 };
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = { -8, -1, 1, 8 };
 
-    protected Bishop(int piecePosition, Alliance pieceAlliance) {
+    protected Rook(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
-    @Override
     public Collection<Move> calculateLegalMoves(final Board board) {
 
         final List<Move> legalMoves = new ArrayList<>();
@@ -57,10 +56,10 @@ public class Bishop extends Piece {
     }
 
     private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.FIRST_COLUMN[currentPosition] && candidateOffset == -1;
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == 7);
     }
 
     private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset) {
-        return BoardUtils.EIGHTH_COLUMN[currentPosition] && candidateOffset == 1;
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 9);
     }
 }
