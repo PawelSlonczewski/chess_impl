f5aab83c98b46e855f3ebfcbff7a461804f4d1a9
PawelSlonczewski <p.slonczewski@student.uksw.edu.pl>
2024-12-23 23:05:56 +0100

    developed board class a bit and made pieces classes constructors public

--- src/main/java/com/pslonczewski/chad_chess_variant_impl/engine/pieces/Queen.java
+++ src/main/java/com/pslonczewski/chad_chess_variant_impl/engine/pieces/Queen.java
@@ -12,7 +12,7 @@ public class Queen extends Piece {
 
     private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = { -9, -8, -7, -1, 1, 7, 8, 9 };
 
-    protected Queen(int piecePosition, Alliance pieceAlliance) {
+    public Queen(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
--- src/main/java/com/pslonczewski/chad_chess_variant_impl/engine/pieces/Rook.java
+++ src/main/java/com/pslonczewski/chad_chess_variant_impl/engine/pieces/Rook.java
@@ -12,7 +12,7 @@ public class Rook extends Piece{
 
     private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = { -8, -1, 1, 8 };
 
-    protected Rook(int piecePosition, Alliance pieceAlliance) {
+    public Rook(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
